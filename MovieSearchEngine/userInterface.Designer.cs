a61bbcd859401df6a74d75b9afc7bc0b82603ac3
Jie Zheng <jayie21@live.com>
2018-11-25 16:16:37 -0600

    Add movie detail box

--- MovieSearchEngine/userInterface.Designer.cs
+++ MovieSearchEngine/userInterface.Designer.cs
@@ -33,8 +33,9 @@
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // textBox1
@@ -48,7 +49,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(325, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.Size = new System.Drawing.Size(158, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,40 +59,30 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(12, 40);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(98, 21);
+            this.checkBox1.Size = new System.Drawing.Size(108, 21);
             this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "Movie Name";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(116, 40);
+            this.checkBox2.Location = new System.Drawing.Point(126, 40);
             this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(98, 21);
+            this.checkBox2.Size = new System.Drawing.Size(104, 21);
             this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.Text = "Actor Name";
             this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(221, 41);
+            this.checkBox3.Location = new System.Drawing.Point(236, 40);
             this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(98, 21);
+            this.checkBox3.Size = new System.Drawing.Size(121, 21);
             this.checkBox3.TabIndex = 4;
-            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.Text = "Director Name";
             this.checkBox3.UseVisualStyleBackColor = true;
-            // 
-            // checkBox4
-            // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Location = new System.Drawing.Point(326, 40);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(98, 21);
-            this.checkBox4.TabIndex = 5;
-            this.checkBox4.Text = "checkBox4";
-            this.checkBox4.UseVisualStyleBackColor = true;
             // 
             // listBox1
             // 
@@ -99,16 +90,33 @@
             this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(12, 67);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(427, 180);
+            this.listBox1.Size = new System.Drawing.Size(218, 292);
             this.listBox1.TabIndex = 6;
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.Location = new System.Drawing.Point(363, 41);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.domainUpDown1.TabIndex = 7;
+            this.domainUpDown1.Text = "Genere";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(245, 67);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(392, 288);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // userInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(451, 253);
+            this.ClientSize = new System.Drawing.Size(649, 367);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.domainUpDown1);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.checkBox4);
             this.Controls.Add(this.checkBox3);
             this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.checkBox1);
@@ -128,8 +136,9 @@
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.CheckBox checkBox3;
-        private System.Windows.Forms.CheckBox checkBox4;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
