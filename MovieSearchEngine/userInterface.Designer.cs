c001c30411b6087a7eede64902a85044b1333c25
Jie Zheng <jayie21@live.com>
2018-11-25 16:43:58 -0600

    Minor changes

--- MovieSearchEngine/userInterface.Designer.cs
+++ MovieSearchEngine/userInterface.Designer.cs
@@ -28,61 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
-            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.uxSearchBox = new System.Windows.Forms.TextBox();
+            this.uxSearchButton = new System.Windows.Forms.Button();
+            this.uxMovieNameCheck = new System.Windows.Forms.CheckBox();
+            this.uxActorNameCheck = new System.Windows.Forms.CheckBox();
+            this.uxDirectorNameCheck = new System.Windows.Forms.CheckBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
-            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            this.uxGenereList = new System.Windows.Forms.DomainUpDown();
             this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
-            // textBox1
+            // uxSearchBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(307, 22);
-            this.textBox1.TabIndex = 0;
+            this.uxSearchBox.Location = new System.Drawing.Point(12, 12);
+            this.uxSearchBox.Name = "uxSearchBox";
+            this.uxSearchBox.Size = new System.Drawing.Size(307, 22);
+            this.uxSearchBox.TabIndex = 0;
             // 
-            // button1
+            // uxSearchButton
             // 
-            this.button1.Location = new System.Drawing.Point(325, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(158, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Search";
-            this.button1.UseVisualStyleBackColor = true;
+            this.uxSearchButton.Location = new System.Drawing.Point(325, 12);
+            this.uxSearchButton.Name = "uxSearchButton";
+            this.uxSearchButton.Size = new System.Drawing.Size(158, 23);
+            this.uxSearchButton.TabIndex = 1;
+            this.uxSearchButton.Text = "Search";
+            this.uxSearchButton.UseVisualStyleBackColor = true;
+            this.uxSearchButton.Click += new System.EventHandler(this.uxSearchButton_Click);
             // 
-            // checkBox1
+            // uxMovieNameCheck
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 40);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(108, 21);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "Movie Name";
-            this.checkBox1.UseVisualStyleBackColor = true;
+            this.uxMovieNameCheck.AutoSize = true;
+            this.uxMovieNameCheck.Location = new System.Drawing.Point(12, 40);
+            this.uxMovieNameCheck.Name = "uxMovieNameCheck";
+            this.uxMovieNameCheck.Size = new System.Drawing.Size(108, 21);
+            this.uxMovieNameCheck.TabIndex = 2;
+            this.uxMovieNameCheck.Text = "Movie Name";
+            this.uxMovieNameCheck.UseVisualStyleBackColor = true;
             // 
-            // checkBox2
+            // uxActorNameCheck
             // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(126, 40);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(104, 21);
-            this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "Actor Name";
-            this.checkBox2.UseVisualStyleBackColor = true;
+            this.uxActorNameCheck.AutoSize = true;
+            this.uxActorNameCheck.Location = new System.Drawing.Point(126, 40);
+            this.uxActorNameCheck.Name = "uxActorNameCheck";
+            this.uxActorNameCheck.Size = new System.Drawing.Size(104, 21);
+            this.uxActorNameCheck.TabIndex = 3;
+            this.uxActorNameCheck.Text = "Actor Name";
+            this.uxActorNameCheck.UseVisualStyleBackColor = true;
             // 
-            // checkBox3
+            // uxDirectorNameCheck
             // 
-            this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(236, 40);
-            this.checkBox3.Name = "checkBox3";
-            this.checkBox3.Size = new System.Drawing.Size(121, 21);
-            this.checkBox3.TabIndex = 4;
-            this.checkBox3.Text = "Director Name";
-            this.checkBox3.UseVisualStyleBackColor = true;
+            this.uxDirectorNameCheck.AutoSize = true;
+            this.uxDirectorNameCheck.Location = new System.Drawing.Point(236, 40);
+            this.uxDirectorNameCheck.Name = "uxDirectorNameCheck";
+            this.uxDirectorNameCheck.Size = new System.Drawing.Size(121, 21);
+            this.uxDirectorNameCheck.TabIndex = 4;
+            this.uxDirectorNameCheck.Text = "Director Name";
+            this.uxDirectorNameCheck.UseVisualStyleBackColor = true;
             // 
             // listBox1
             // 
@@ -93,13 +94,13 @@
             this.listBox1.Size = new System.Drawing.Size(218, 292);
             this.listBox1.TabIndex = 6;
             // 
-            // domainUpDown1
+            // uxGenereList
             // 
-            this.domainUpDown1.Location = new System.Drawing.Point(363, 41);
-            this.domainUpDown1.Name = "domainUpDown1";
-            this.domainUpDown1.Size = new System.Drawing.Size(120, 22);
-            this.domainUpDown1.TabIndex = 7;
-            this.domainUpDown1.Text = "Genere";
+            this.uxGenereList.Location = new System.Drawing.Point(363, 41);
+            this.uxGenereList.Name = "uxGenereList";
+            this.uxGenereList.Size = new System.Drawing.Size(120, 22);
+            this.uxGenereList.TabIndex = 7;
+            this.uxGenereList.Text = "Genere";
             // 
             // listView1
             // 
@@ -115,13 +116,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(649, 367);
             this.Controls.Add(this.listView1);
-            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.uxGenereList);
             this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.checkBox3);
-            this.Controls.Add(this.checkBox2);
-            this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.uxDirectorNameCheck);
+            this.Controls.Add(this.uxActorNameCheck);
+            this.Controls.Add(this.uxMovieNameCheck);
+            this.Controls.Add(this.uxSearchButton);
+            this.Controls.Add(this.uxSearchBox);
             this.Name = "userInterface";
             this.Text = "Movie Search Engine";
             this.ResumeLayout(false);
@@ -131,13 +132,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.TextBox uxSearchBox;
+        private System.Windows.Forms.Button uxSearchButton;
+        private System.Windows.Forms.CheckBox uxMovieNameCheck;
+        private System.Windows.Forms.CheckBox uxActorNameCheck;
+        private System.Windows.Forms.CheckBox uxDirectorNameCheck;
         private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.DomainUpDown uxGenereList;
         private System.Windows.Forms.ListView listView1;
     }
 }
--- MovieSearchEngine/userInterface.cs
+++ MovieSearchEngine/userInterface.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,34 @@ namespace MovieSearchEngine
         public userInterface()
         {
             InitializeComponent();
+        }
+
+        private void uxSearchButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Build connection string
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "localhost";   // update me
+                builder.UserID = "sa";              // update me
+                builder.Password = "your_password";      // update me
+                builder.InitialCatalog = "master";
+
+                // Connect to SQL
+                uxSearchBox.Text = "Connecting to SQL Server ... ";
+                //Console.Write("Connecting to SQL Server ... ");
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    connection.Open();
+                    //Console.WriteLine("Done.");
+                    uxSearchBox.Text = "Done";
+                }
+            }
+            catch (SqlException ex)
+            {
+                uxSearchBox.Text = ex.ToString();
+                //Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
