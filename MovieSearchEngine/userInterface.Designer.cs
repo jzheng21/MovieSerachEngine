63d236ca9a5ac9560bac79d9ecbbff442b9134fa
Jie Zheng <jayie21@ksu.edu>
2018-12-02 20:45:57 -0600

    Add query

--- MovieSearchEngine/userInterface.Designer.cs
+++ MovieSearchEngine/userInterface.Designer.cs
@@ -28,129 +28,221 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.uxSearchBox = new System.Windows.Forms.TextBox();
-            this.uxSearchButton = new System.Windows.Forms.Button();
-            this.uxMovieNameCheck = new System.Windows.Forms.CheckBox();
-            this.uxActorNameCheck = new System.Windows.Forms.CheckBox();
-            this.uxDirectorNameCheck = new System.Windows.Forms.CheckBox();
+            this.uxMovieNameTextbox = new System.Windows.Forms.TextBox();
             this.movieFoundListbox = new System.Windows.Forms.ListBox();
             this.listView1 = new System.Windows.Forms.ListView();
+            this.uxSearchButton = new System.Windows.Forms.Button();
+            this.uxMovieNameLabel = new System.Windows.Forms.Label();
+            this.uxActorNameLabel = new System.Windows.Forms.Label();
+            this.uxDirectorNameLabel = new System.Windows.Forms.Label();
+            this.uxReleaseYearLabel = new System.Windows.Forms.Label();
+            this.uxActorNameTextbox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.uxGenreLabel = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // uxSearchBox
+            // uxMovieNameTextbox
             // 
-            this.uxSearchBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.uxSearchBox.Location = new System.Drawing.Point(18, 19);
-            this.uxSearchBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.uxSearchBox.Name = "uxSearchBox";
-            this.uxSearchBox.Size = new System.Drawing.Size(458, 34);
-            this.uxSearchBox.TabIndex = 0;
+            this.uxMovieNameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxMovieNameTextbox.Location = new System.Drawing.Point(128, 5);
+            this.uxMovieNameTextbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uxMovieNameTextbox.Name = "uxMovieNameTextbox";
+            this.uxMovieNameTextbox.Size = new System.Drawing.Size(201, 29);
+            this.uxMovieNameTextbox.TabIndex = 0;
+            // 
+            // movieFoundListbox
+            // 
+            this.movieFoundListbox.FormattingEnabled = true;
+            this.movieFoundListbox.ItemHeight = 20;
+            this.movieFoundListbox.Location = new System.Drawing.Point(13, 288);
+            this.movieFoundListbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.movieFoundListbox.Name = "movieFoundListbox";
+            this.movieFoundListbox.Size = new System.Drawing.Size(367, 464);
+            this.movieFoundListbox.TabIndex = 6;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(388, 288);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(492, 316);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // uxSearchButton
             // 
-            this.uxSearchButton.Location = new System.Drawing.Point(488, 19);
+            this.uxSearchButton.Location = new System.Drawing.Point(13, 242);
             this.uxSearchButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.uxSearchButton.Name = "uxSearchButton";
-            this.uxSearchButton.Size = new System.Drawing.Size(237, 36);
+            this.uxSearchButton.Size = new System.Drawing.Size(367, 36);
             this.uxSearchButton.TabIndex = 1;
             this.uxSearchButton.Text = "Search";
             this.uxSearchButton.UseVisualStyleBackColor = true;
             this.uxSearchButton.Click += new System.EventHandler(this.uxSearchButton_Click);
             // 
-            // uxMovieNameCheck
+            // uxMovieNameLabel
             // 
-            this.uxMovieNameCheck.AutoSize = true;
-            this.uxMovieNameCheck.Location = new System.Drawing.Point(18, 63);
-            this.uxMovieNameCheck.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.uxMovieNameCheck.Name = "uxMovieNameCheck";
-            this.uxMovieNameCheck.Size = new System.Drawing.Size(144, 29);
-            this.uxMovieNameCheck.TabIndex = 2;
-            this.uxMovieNameCheck.Text = "Movie Name";
-            this.uxMovieNameCheck.UseVisualStyleBackColor = true;
+            this.uxMovieNameLabel.AutoSize = true;
+            this.uxMovieNameLabel.Location = new System.Drawing.Point(3, 0);
+            this.uxMovieNameLabel.Name = "uxMovieNameLabel";
+            this.uxMovieNameLabel.Size = new System.Drawing.Size(96, 20);
+            this.uxMovieNameLabel.TabIndex = 9;
+            this.uxMovieNameLabel.Text = "Movie Name";
             // 
-            // uxActorNameCheck
+            // uxActorNameLabel
             // 
-            this.uxActorNameCheck.AutoSize = true;
-            this.uxActorNameCheck.Location = new System.Drawing.Point(189, 63);
-            this.uxActorNameCheck.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.uxActorNameCheck.Name = "uxActorNameCheck";
-            this.uxActorNameCheck.Size = new System.Drawing.Size(137, 29);
-            this.uxActorNameCheck.TabIndex = 3;
-            this.uxActorNameCheck.Text = "Actor Name";
-            this.uxActorNameCheck.UseVisualStyleBackColor = true;
+            this.uxActorNameLabel.AutoSize = true;
+            this.uxActorNameLabel.Location = new System.Drawing.Point(3, 43);
+            this.uxActorNameLabel.Name = "uxActorNameLabel";
+            this.uxActorNameLabel.Size = new System.Drawing.Size(93, 20);
+            this.uxActorNameLabel.TabIndex = 10;
+            this.uxActorNameLabel.Text = "Actor Name";
             // 
-            // uxDirectorNameCheck
+            // uxDirectorNameLabel
             // 
-            this.uxDirectorNameCheck.AutoSize = true;
-            this.uxDirectorNameCheck.Location = new System.Drawing.Point(354, 63);
-            this.uxDirectorNameCheck.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.uxDirectorNameCheck.Name = "uxDirectorNameCheck";
-            this.uxDirectorNameCheck.Size = new System.Drawing.Size(158, 29);
-            this.uxDirectorNameCheck.TabIndex = 4;
-            this.uxDirectorNameCheck.Text = "Director Name";
-            this.uxDirectorNameCheck.UseVisualStyleBackColor = true;
+            this.uxDirectorNameLabel.AutoSize = true;
+            this.uxDirectorNameLabel.Location = new System.Drawing.Point(3, 86);
+            this.uxDirectorNameLabel.Name = "uxDirectorNameLabel";
+            this.uxDirectorNameLabel.Size = new System.Drawing.Size(111, 20);
+            this.uxDirectorNameLabel.TabIndex = 11;
+            this.uxDirectorNameLabel.Text = "Director Name";
             // 
-            // movieFoundListbox
+            // uxReleaseYearLabel
             // 
-            this.movieFoundListbox.FormattingEnabled = true;
-            this.movieFoundListbox.ItemHeight = 25;
-            this.movieFoundListbox.Location = new System.Drawing.Point(18, 105);
-            this.movieFoundListbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.movieFoundListbox.Name = "movieFoundListbox";
-            this.movieFoundListbox.Size = new System.Drawing.Size(325, 454);
-            this.movieFoundListbox.TabIndex = 6;
+            this.uxReleaseYearLabel.AutoSize = true;
+            this.uxReleaseYearLabel.Location = new System.Drawing.Point(3, 129);
+            this.uxReleaseYearLabel.Name = "uxReleaseYearLabel";
+            this.uxReleaseYearLabel.Size = new System.Drawing.Size(43, 20);
+            this.uxReleaseYearLabel.TabIndex = 12;
+            this.uxReleaseYearLabel.Text = "Year";
             // 
-            // listView1
+            // uxActorNameTextbox
             // 
-            this.listView1.Location = new System.Drawing.Point(368, 105);
-            this.listView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(586, 448);
-            this.listView1.TabIndex = 8;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.uxActorNameTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxActorNameTextbox.Location = new System.Drawing.Point(128, 48);
+            this.uxActorNameTextbox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uxActorNameTextbox.Name = "uxActorNameTextbox";
+            this.uxActorNameTextbox.Size = new System.Drawing.Size(201, 29);
+            this.uxActorNameTextbox.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(128, 91);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 29);
+            this.textBox2.TabIndex = 15;
+            // 
+            // uxGenreLabel
+            // 
+            this.uxGenreLabel.AutoSize = true;
+            this.uxGenreLabel.Location = new System.Drawing.Point(3, 172);
+            this.uxGenreLabel.Name = "uxGenreLabel";
+            this.uxGenreLabel.Size = new System.Drawing.Size(54, 20);
+            this.uxGenreLabel.TabIndex = 16;
+            this.uxGenreLabel.Text = "Genre";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(128, 134);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(201, 29);
+            this.textBox4.TabIndex = 18;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.85417F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.14584F));
+            this.tableLayoutPanel1.Controls.Add(this.uxMovieNameLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.uxActorNameLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.uxGenreLabel, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.uxActorNameTextbox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.uxReleaseYearLabel, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.uxMovieNameTextbox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.uxDirectorNameLabel, 0, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 19);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(367, 215);
+            this.tableLayoutPanel1.TabIndex = 19;
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(388, 19);
+            this.listView2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(492, 259);
+            this.listView2.TabIndex = 20;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(387, 612);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(493, 100);
+            this.textBox5.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(782, 718);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "None",
-            "Comedy"});
-            this.comboBox1.Location = new System.Drawing.Point(600, 63);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            "All",
+            "Comedy",
+            "Action",
+            "Advanture"});
+            this.comboBox1.Location = new System.Drawing.Point(127, 175);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(92, 33);
-            this.comboBox1.TabIndex = 9;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(519, 66);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 25);
-            this.label1.TabIndex = 10;
-            this.label1.Text = "Genere";
+            this.comboBox1.Size = new System.Drawing.Size(202, 28);
+            this.comboBox1.TabIndex = 19;
             // 
             // userInterface
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(974, 573);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(890, 774);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.movieFoundListbox);
-            this.Controls.Add(this.uxDirectorNameCheck);
-            this.Controls.Add(this.uxActorNameCheck);
-            this.Controls.Add(this.uxMovieNameCheck);
             this.Controls.Add(this.uxSearchButton);
-            this.Controls.Add(this.uxSearchBox);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "userInterface";
             this.Text = "Movie Search Engine";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,15 +250,23 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox uxSearchBox;
-        private System.Windows.Forms.Button uxSearchButton;
-        private System.Windows.Forms.CheckBox uxMovieNameCheck;
-        private System.Windows.Forms.CheckBox uxActorNameCheck;
-        private System.Windows.Forms.CheckBox uxDirectorNameCheck;
+        private System.Windows.Forms.TextBox uxMovieNameTextbox;
         private System.Windows.Forms.ListBox movieFoundListbox;
         private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button uxSearchButton;
+        private System.Windows.Forms.Label uxMovieNameLabel;
+        private System.Windows.Forms.Label uxActorNameLabel;
+        private System.Windows.Forms.Label uxDirectorNameLabel;
+        private System.Windows.Forms.Label uxReleaseYearLabel;
+        private System.Windows.Forms.TextBox uxActorNameTextbox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label uxGenreLabel;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label1;
     }
 }
 
